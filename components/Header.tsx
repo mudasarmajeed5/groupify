d331c5089abe14647f1de13b47b70264a52bbc98
mudasarmajeed5 <mudasarmajeed5@yahoo.com>
2025-07-29 16:09:00 +0500

    verified signature

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,6 +19,7 @@ const Header = async () => {
                     </> : <Button onClick={() => redirect("/login")}>Login</Button>
                 }
                 <Button>Invite</Button>
+                
             </div>
         </nav>
     )
